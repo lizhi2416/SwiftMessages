bd48486c4bfc27c155c699e34c8b567c84623322
jlzgit <275878610@qq.com>
2021-12-29 19:29:46 +0800

    nothing

---
+++ Demo/Demo/MYViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MYViewController.swift
+//  CustomSegueDemo
+//
+//  Created by 蒋理智 on 2021/4/29.
+//  Copyright © 2021 it.swiftkick. All rights reserved.
+//
+
+import UIKit
+
+class MYViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
